71304b150445a7c8f9f1902a3cb77f4d75471f07
aldisatria12 <aldisatria253@gmail.com>
2022-08-24 18:26:43 +0800

    Initial Commit

---
+++ MahotmaGandhi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MahotmaGandhi
+//
+//  Created by Aldi Mahotma on 24/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MahotmaGandhi/MahotmaGandhiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MahotmaGandhiApp.swift
+//  MahotmaGandhi
+//
+//  Created by Aldi Mahotma on 24/08/22.
+//
+
+import SwiftUI
+
+@main
+struct MahotmaGandhiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
