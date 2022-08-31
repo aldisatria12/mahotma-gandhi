6f83506f2f8ebc5f31b1b08fc5f595a5d3294cd3
GTerrence <tpramono@student.ciputra.ac.id>
2022-08-31 11:47:53 +0700

    feat : add all color pallete

--- MahotmaGandhi/Services/ColorPallete.swift
+++ MahotmaGandhi/Services/ColorPallete.swift
@@ -8,3 +8,12 @@
 import SwiftUI
 
 let blue04 = Color("Blue-4")
+let blue03 = Color("Blue-3")
+let blue02 = Color("Blue-2")
+let blue01 = Color("Blue-1")
+let red01 = Color("Red-1")
+let red02 = Color("Red-2")
+let red03 = Color("Red-3")
+let yellow01 = Color("Yellow-1")
+let yellow02 = Color("Yellow-2")
+let yellow03 = Color("Yellow-3")
