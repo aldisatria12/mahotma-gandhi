a1aa108c9a0d043657312d225db4d298bc501de2
aldisatria12 <aldisatria253@gmail.com>
2022-09-08 16:26:43 +0700

    add allplayerview

---
+++ MahotmaGandhi/Player/View/AllPlayerNavBarView.swift
@@ -0,0 +1,23 @@
+//
+//  AllPlayerNavBarView.swift
+//  MahotmaGandhi
+//
+//  Created by Aldi Mahotma on 08/09/22.
+//
+
+import SwiftUI
+
+struct AllPlayerNavBarView: View {
+    var body: some View {
+        HStack{
+            
+        }
+        .frame(width: UIScreen.main.bounds.width * 24 / 390, height: UIScreen.main.bounds.height * 31 / 844)
+    }
+}
+
+struct AllPlayerNavBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        AllPlayerNavBarView()
+    }
+}
---
+++ MahotmaGandhi/Player/View/AllPlayerView.swift
@@ -0,0 +1,21 @@
+//
+//  AllPlayerView.swift
+//  MahotmaGandhi
+//
+//  Created by Aldi Mahotma on 08/09/22.
+//
+
+import SwiftUI
+
+struct AllPlayerView: View {
+    @StateObject var playerViewModel = PlayerViewModel()
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+//struct AllPlayerView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        AllPlayerView()
+//    }
+//}
