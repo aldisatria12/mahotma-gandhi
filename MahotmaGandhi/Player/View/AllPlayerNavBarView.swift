755c3fdbc6bad961932013e6c618ba295472927c
aldisatria12 <aldisatria253@gmail.com>
2022-09-14 18:18:17 +0700

    fix start wwhere no player

--- MahotmaGandhi/Player/View/AllPlayerNavBarView.swift
+++ MahotmaGandhi/Player/View/AllPlayerNavBarView.swift
@@ -35,23 +35,25 @@ struct AllPlayerNavBarView: View {
                     .padding(.leading)
                     
                     Spacer()
-                    NavigationLink {
-                        GameView(vm: GameViewModel(players: allPlayers))
-                    } label: {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 20)
-                                .frame(width: UIScreen.main.bounds.width * 64 / 390, height: UIScreen.main.bounds.height * 36 / 844)
-                                .foregroundColor(blue01)
-                            Text("Start")
-                                .font(.system(size: 16, design: .rounded))
-                                .fontWeight(.bold)
-                                .frame(width: UIScreen.main.bounds.width * 58 / 390, height: UIScreen.main.bounds.height * 30 / 844)
-                                .background(yellow02)
-                                .foregroundColor(blue01)
-                                .clipShape(RoundedRectangle(cornerRadius: 36))
+                    if allPlayers.count > 0 {
+                        NavigationLink {
+                            GameView(vm: GameViewModel(players: allPlayers))
+                        } label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 20)
+                                    .frame(width: UIScreen.main.bounds.width * 64 / 390, height: UIScreen.main.bounds.height * 36 / 844)
+                                    .foregroundColor(blue01)
+                                Text("Start")
+                                    .font(.system(size: 16, design: .rounded))
+                                    .fontWeight(.bold)
+                                    .frame(width: UIScreen.main.bounds.width * 58 / 390, height: UIScreen.main.bounds.height * 30 / 844)
+                                    .background(yellow02)
+                                    .foregroundColor(blue01)
+                                    .clipShape(RoundedRectangle(cornerRadius: 36))
+                            }
                         }
+                        .padding(.trailing)                        
                     }
-                    .padding(.trailing)
                 }
                 Text("\(allPlayers.count) / 6")
                     .font(.system(size: 18, design: .rounded))
